41189824e4815d167b6189b86ccd4c9f07dea1a3
Warley-DesktopCasa <warleyvods@gmail.com>
2020-04-09 21:54:47 -0300

    teste

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,24 @@ void main () {
 
   String rodrigo = "OI MEU NOME É warley";
 
+  void calcular() {
+    setState(() {
+      double nota1 = double.parse(nota1Control.text);
+      double nota2 = double.parse(nota2Control.text);
+      double nota3 = double.parse(nota3Control.text);
+      double nota4 = double.parse(nota4Control.text);
+
+      double nota = (nota1 + nota2 + nota3 + nota4) / 4;
+      print(nota);
+
+      if (nota >= 6) {
+        info = "Aprovado: ${nota.toStringAsPrecision(2)}";
+      } else if (nota < 6) {
+        info = "Reprovado: ${nota.toStringAsPrecision(2)}";
+      }
+    });
+  }
+
+
 }
 
